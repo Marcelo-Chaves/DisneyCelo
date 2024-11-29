723e718578fe8af2d1861655e85fbaa1a3742707
marcelo chaves <marceloluiz.icg@gmail.com>
2024-11-28 23:24:07 -0400

    setup do projeto

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
 
-function styles (){
-    return gulp.src('./src/style/*.scss')
-        .pipe(sass({outputStyle: 'compressed'}))
-        .pipe(gulp.dest('./dist/css'));
+function styles(){
+    return gulp.src('./src/styles/*.scss')
+        .pipe(sass({outputStyle:'compressed'}))
+        .pipe(gulp.dest('./dist/css'));  
 }
 
 exports.default = styles;
 exports.watch = function(){
-    gulp.watch('./src/style/*.scss',gulp.parallel(styles))
+    gulp.watch('src/styles/*.scss',gulp.parallel(styles))
 }
